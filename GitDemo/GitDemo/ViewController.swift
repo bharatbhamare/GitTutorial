9d1a96f116fa531fbfa9d9d769fc8256603de992
kishor.barde <ujwala.atre@triarqhelath.com>
2018-11-01 17:43:52 +0530

    mac 1 changes

--- GitDemo/GitDemo/ViewController.swift
+++ GitDemo/GitDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("modification")
         print("demi cahanges")
+        print ("im mac 2..... im going change project")
         // Do any additional setup after loading the view, typically from a nib.
         demofunc(name: "bharat");
     }
